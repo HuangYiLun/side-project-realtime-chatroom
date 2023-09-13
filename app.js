9f960ae1e723b499a38e29bb762f8272219e421b
HuangYiLun <wk5018102@gmail.com>
2023-09-13 15:35:03 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,33 @@
+//非production環境下，使用dotenv取得.env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+const routes = require('./routes')
+
+
+const app = express()
+const port = process.env.PORT
+
+//setting template engine
+app.engine('hbs', exphbs.engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
+
+
+
+
+
+app.use(express.static('public'))
+app.use(routes)
+
+app.listen(port, () => {
+  console.log( `app is running on http://localhost:${port}`)
+})
+
+
+
