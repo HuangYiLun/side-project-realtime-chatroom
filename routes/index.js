9704d4d26c2d766bf4d6e07ed751cff75bb8f91a
HuangYiLun <wk5018102@gmail.com>
2023-10-14 15:29:12 +0800

    feat: add signin.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const userController = require('../controllers/user-controller')
+
+
+
+
+router.get('/signin', userController.getSignIn)
+
 router.get('/', (req, res) => res.render('index'))
 
 module.exports = router
