689c737e2c9915daea2495c89a4195500647deaf
HuangYiLun <wk5018102@gmail.com>
2023-10-15 00:36:26 +0800

    feat: add error-handlebars

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,18 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 
 // 登入登出
 router.get('/signin', userController.getSignIn)
-router.post('/signin', passport.authenticate('local', {failureRedirect: '/signin'}), userController.signin)
-router.post('/signout', userController.signout )
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin' }), userController.signin)
+router.post('/signout', userController.signout)
 
 // 註冊
 router.get('/signup', userController.getSignUp)
 
+router.use('/', generalErrorHandler)
 router.get('/', (req, res) => res.render('index'))
 
 module.exports = router
